97f376cd17d9918ace6c60e663c8d5f0a4bcaff2
Zack McCartney <zemccartney@gmail.com>
2025-05-03 16:11:54 -0400

    Convert tabs to standards, remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 // @ts-check
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind(), mdx()],
 });
