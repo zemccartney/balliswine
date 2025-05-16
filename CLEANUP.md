688ca349da62d44b02a3c1bd8b8dac4e295574ac
Zack McCartney <zemccartney@gmail.com>
2025-05-15 21:34:51 -0400

    [CF-Pages-Skip] WIP

---
+++ CLEANUP.md
@@ -0,0 +1,8 @@
+## Wine detail
+
+- wine color passing / fine tune wine colors
+- actually understand cork image / pattern
+  - https://css-tricks.com/creating-patterns-with-svg-filters/
+  - https://css-tricks.com/grainy-gradients/
+- actually understand filter usage and drop shadow vs shadow
+- try replicating wood grain pattern from: https://codepen.io/lynnandtonic/pen/LYGjqOo
---
+++ src/pages/cellar/stub.ts
@@ -0,0 +1,44 @@
+export default [
+  {
+    hue: "bg-rose-900",
+    id: "alosa",
+    name: "Alosa",
+    producer: "Cellar Comalats",
+  },
+  {
+    hue: "bg-rose-900",
+    id: "avanti-popolo",
+    name: "Avanti Popolo",
+    producer: "Cantina Furlani",
+  },
+  {
+    hue: "bg-rose-900",
+    id: "el-marciano",
+    name: "El Marciano",
+    producer: "Alfredo Maestro",
+  },
+  {
+    hue: "bg-rose-700",
+    id: "bel-soula",
+    name: "Bel Soula",
+    producer: "Domaine Sulauze",
+  },
+  {
+    hue: "bg-yellow-200",
+    id: "pur-breton",
+    name: "Pur Breton",
+    producer: "Domaine Bobinet",
+  },
+  {
+    hue: "bg-rose-300",
+    id: "vin-de-frantz",
+    name: "Vin de Frantz",
+    producer: "Frantz Saumon",
+  },
+  {
+    hue: "bg-rose-900",
+    id: "vinhota",
+    name: "Vinhota",
+    producer: "Quinta da Boavista",
+  },
+];
