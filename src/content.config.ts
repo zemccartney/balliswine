adc8edd07d9c1b02625dedc31c220c1500d3bc00
Zack McCartney <zemccartney@gmail.com>
2025-05-21 20:10:27 -0400

    [CF-Pages-Skip] WIP

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,25 +1,60 @@
-import { glob } from "astro/loaders";
+import { file, glob } from "astro/loaders";
 import { defineCollection, reference, z } from "astro:content";
 
-const daysCollection = defineCollection({
-  loader: glob({ base: "./src/content/days", pattern: "**/[^_]*.{md,mdx}" }),
-  schema: z.object({
-    ballContent: reference("thoughts"),
-    wineContent: reference("thoughts"),
-  }),
-});
-
-const thoughtsCollection = defineCollection({
+const producers = defineCollection({
   loader: glob({
-    base: "./src/content/thoughts",
+    base: "./src/content/producers",
     pattern: "**/[^_]*.{md,mdx}",
   }),
   schema: z.object({
-    wine: z.string().optional(),
+    coordinates: z.tuple([z.number(), z.number()]),
+    location: z.string(),
+    name: z.string(),
+  }),
+});
+
+/*
+TODO
+- need to source hue values, figure out passage to bottle display
+- figure out parsing post intro and body
+- figure out how to dynamically load images
+*/
+
+const wines = defineCollection({
+  loader: file("src/content/wines.json"),
+  schema: z.object({
+    hue: z.string(),
+    name: z.string(),
+    producer: reference("producers"),
+  }),
+});
+
+const vintages = defineCollection({
+  loader: glob({
+    base: "./src/content/vintages",
+    pattern: "**/[^_]*.{md,mdx}",
+  }),
+  schema: z.object({
+    cepage: z.string(),
+    dateTried: z.string(),
+    // tasting notes is an md or mdx file
+  }),
+});
+
+const posts = defineCollection({
+  loader: glob({
+    base: "./src/content/vintages",
+    pattern: "**/[^_]*.{md,mdx}",
+  }),
+  schema: z.object({
+    wine: reference("vintages"),
+    // tasting notes is an md or mdx file
   }),
 });
 
 export const collections = {
-  days: daysCollection,
-  thoughts: thoughtsCollection,
+  posts,
+  producers,
+  vintages,
+  wines,
 };
---
+++ src/content/producers/domain-balansa.md
@@ -0,0 +1,7 @@
+---
+coordinates: 42.976677, 2.779531
+location: Villeneuve-les-Corbières, France
+name: Domaine Balansa
+---
+
+[Pay them a visit](https://www.domainebalansa.com/)
