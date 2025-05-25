2d140f1844a81e1be44fdf1a25835ec1fd2b498f
Zack McCartney <zemccartney@gmail.com>
2025-05-25 13:14:41 -0400

    Explicitly load leaflet images to expose to bundler

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    // leaflet and youtube-lite are prone to disappearing, failure to re-optimize; unclear why, try commenting this
+    // out and rerunning build instead of clearing node_modules and reinstalling
+    optimizeDeps: {
+      force: true,
+    },
+
     plugins: [tailwindcss()],
   },
 });
